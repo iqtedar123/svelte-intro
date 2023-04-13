0d02d18bc7d491eb0e9e0d083bae310d2ab389ef
Mohammad Chowdhry <mohammad.chowdhry@pcbank.ca>
2023-04-13 15:36:42 -0400

    add gitignore

---
+++ src/components/ReactDiagram.svelte
@@ -0,0 +1,5 @@
+<script>
+  import diagram from "../static/react-diagram.webp";
+</script>
+
+<img src={diagram} width="100%" alt="React-Svelte-Diagram" class="mb-8" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,23 @@
 <script>
-	import Feature from '../components/Feature/feature.svelte';
-	import Hero from '../components/Hero/hero.svelte';
-	import StackBlitz from '../components/StackBlitz.svelte';
-	import { SvelteFeatures } from '../static/svelteFeatures';
+  import Feature from "../components/Feature/feature.svelte";
+  import Hero from "../components/Hero/hero.svelte";
+  import ReactDiagram from "../components/ReactDiagram.svelte";
+  import StackBlitz from "../components/StackBlitz.svelte";
+  import { SvelteFeatures } from "../static/svelteFeatures";
 </script>
 
 <svelte:head>
-	<title>Welcome to Svelte</title>
+  <title>Welcome to Svelte</title>
 </svelte:head>
 <Hero title="/ˈsvɛlt/" subtitle="attractively thin, graceful and stylish" />
 <section id="features">
-	<Feature
-		title="Features"
-		subtitle="CYBERNETICALLY ENHANCED WEB APPS"
-		description="Svelte is a radical new approach to building user interfaces. Whereas traditional frameworks like React and Vue do the bulk of their work in the browser, Svelte shifts that work into a compile step that happens when you build your app. Instead of using techniques like virtual DOM diffing, Svelte writes code that surgically updates the DOM when the state of your app changes."
-		features={SvelteFeatures}
-	/>
+  <Feature
+    title="Features"
+    subtitle="CYBERNETICALLY ENHANCED WEB APPS"
+    description="Svelte is a radical new approach to building user interfaces. Whereas traditional frameworks like React and Vue do the bulk of their work in the browser, Svelte shifts that work into a compile step that happens when you build your app. Instead of using techniques like virtual DOM diffing, Svelte writes code that surgically updates the DOM when the state of your app changes."
+    features={SvelteFeatures}
+  />
+  <ReactDiagram />
 </section>
 
 <StackBlitz />
