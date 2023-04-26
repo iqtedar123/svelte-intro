7811dd77ba1d487f30256e17087c5996c1c47ce5
Mohammad Chowdhry <mohammad.chowdhry@pcbank.ca>
2023-04-26 13:03:07 -0400

    update gitignore

--- src/components/SharedState.svelte
+++ src/components/SharedState.svelte
@@ -1,23 +1,28 @@
 <div class="bg-white py-24 sm:py-32">
-	<div class="mx-auto max-w-7xl px-6 lg:px-8">
-		<div class="mx-auto max-w-2xl lg:text-center">
-			<h2 class="text-base font-semibold leading-7 text-indigo-600">Redux? Whats that???</h2>
-			<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-				Global Shared State
-			</p>
-			<p class="mt-6 text-lg leading-8 text-gray-600">
-				In Svelte, to pass data from one component down to its children (props), you need to use the <code
-					>export</code
-				> keyword.
-			</p>
-		</div>
-	</div>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:text-center">
+      <h2 class="text-base font-semibold leading-7 text-indigo-600">
+        Redux? Whats that???
+      </h2>
+      <p
+        class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+      >
+        Global Shared State
+      </p>
+      <p class="mt-6 text-lg leading-8 text-gray-600">
+        No third party library required. Svelte has sharable state built in
+        using <code>stores</code>. A store is simply an object with a
+        <code>subscribe</code> method that allows interested parties to be notified
+        whenever the store value changes.
+      </p>
+    </div>
+  </div>
 </div>
 <div class="mb-8 w-full">
-	<iframe
-		src="https://stackblitz.com/edit/vitejs-vite-s2nyot?embed=1&file=src/App.svelte&hideNavigation=1&theme=light"
-		width="100%"
-		height="500px"
-		title="SharedState"
-	/>
+  <iframe
+    src="https://stackblitz.com/edit/vitejs-vite-s2nyot?embed=1&file=src/App.svelte&hideNavigation=1&theme=light"
+    width="100%"
+    height="500px"
+    title="SharedState"
+  />
 </div>
